7974110ea1cf904139df07877c3a8dee0294b08b
andresgil2809 <andresgil3010@hotmail.com>
2023-03-07 16:45:49 -0500

    modificacion de botones dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,8 @@
         <div class="w-6x1 mx-auto hx-auto sm:px-6 container">
             <div class=" bg-gray-100 overflow-hidden shadow-xl sm:rounded-lg text-start flex p-6 h-4x1" >
                 
-                <div class="terreno flex-1 text-center ">
-                    <h2 class="h2 font-extrabold  uppercase text-black " >Gestion de cultivos</h2>
+                <div class="terreno flex-1">
+                    <h2 class="h2 font-extrabold text-center uppercase text-black " >Gestion de cultivos</h2>
 
                     <ul class="space-y-1 text-center">
                         <li><a href="{{ route('plantas.create') }}" class="w-50  bg-gray-800 hover:bg-gray-400 text-white border border-solid-black hover:bg-gray-700 font-bold py-2 px-4 rounded inline-flex hover:bg-gray-400 transition items-center">Registrar una planta  </a></li>
@@ -20,14 +20,21 @@
                     </ul>
                     
                 </div>
-                <div class="ganado flex-1 text-center">
-                    <h2 class=" h2 font-extrabold uppercase  text-black " >Gestion de ganado</h2>
-                    <ul class="space-y-1">
-                        <li><a href="{{ route('animal.create') }}" class="w-50 bg-gray-800 hover:bg-gray-400 text-white border border-solid-black hover:bg-gray-700 font-bold py-2 px-4 rounded inline-flex hover:bg-gray-400 transition items-center">Agregar un nuevo animal</a></li>
+                <div class="ganado flex-1">
+                    <h2 class=" h2 font-extrabold uppercase text-center text-black " >Gestion de ganado</h2>
+                    <ul class="space-y-1 text-center">
+                        <li><button type="button" class="btn btn-dark btn-lg btn-block"><a href="{{ route('animal.create') }}" class="text-white ">Agregar un nuevo animal</a></button></li>
+                        <li><button type="button" class="btn btn-dark btn-lg btn-block"><a href="{{ route('animal.index') }}" class="text-white">Listado de animales</a></button></li>
+                        <li><button type="button" class="btn btn-dark" ><a href="{{ route('production.index') }}" class="text-white">Producción</a></button></li>
+                        <li><button type="button" class="btn btn-dark"><a href="{{ route('healths.index') }}" class="text-white">Sanidad</a></button></li>
+                        <li><button type="button" class="btn btn-dark"><a href="{{ route('foods.index') }}" class="text-white">Comida</a></button></li>
+                        
+
+                        {{-- <li><a href="{{ route('animal.create') }}" class="w-50 bg-gray-800 hover:bg-gray-400 text-white border border-solid-black hover:bg-gray-700 font-bold py-2 px-4 rounded inline-flex hover:bg-gray-400 transition items-center">Agregar un nuevo animal</a></li>
                         <li><a href="{{ route('animal.index') }}" class="w-50 bg-gray-800 hover:bg-gray-400 text-white border border-solid-black hover:bg-gray-700 font-bold py-2 px-4 rounded inline-flex hover:bg-gray-400 transition items-center">Listado de animales</a></li>
                         <li><a class="w-50 bg-gray-800 hover:bg-gray-400 text-white border border-solid-black hover:bg-gray-700 font-bold py-2 px-4 rounded inline-flex hover:bg-gray-400 transition items-center " href="{{ route('production.index') }}">Produccion</a></li>
                         <li><a class="w-50 bg-gray-800 hover:bg-gray-400 text-white border border-solid-black hover:bg-gray-700 font-bold py-2 px-4 rounded inline-flex hover:bg-gray-400 transition items-center " href="{{ route('healths.index') }}">Sanidad</a></li>
-                        <li><a class="w-50 bg-gray-800 hover:bg-gray-400 text-white border border-solid-black hover:bg-gray-700 font-bold py-2 px-4 rounded inline-flex hover:bg-gray-400 transition items-center " href="{{ route('foods.index') }}">Comida</a></li>
+                        <li><a class="w-50 bg-gray-800 hover:bg-gray-400 text-white border border-solid-black hover:bg-gray-700 font-bold py-2 px-4 rounded inline-flex hover:bg-gray-400 transition items-center " href="{{ route('foods.index') }}">Comida</a></li> --}}
                     </ul>
                     
                 </div>
